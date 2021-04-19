f1145f854653f30eeb39f84a172cbd5af0c2fc59
322160 <bagherifar.kamyab@stud.th-luebeck.de>
2021-04-19 17:31:06 +0200

    done_6

--- src/Comprehension.hs
+++ src/Comprehension.hs
@@ -1,18 +1,26 @@
 module Comprehension where
 import Prelude hiding (map,filter)
 
+
 xs =  [x^2 | x <- [1..9], mod x 2==0 ]
 
-sqrs = [x^2 | x <- [0..6], mod x 2==0]
 
-euler1 = sum[x | x <- [0..999], mod x 3 == 0 || mod x 5 == 0 ]
+sqrs = [x^2 | x <- [0..6], mod x 2 == 0 , x > 0]
 
-prodsOdd = [x * y | x <- [1..3], y <- [2..5], mod (x + y) 2 == 1]
+
+euler = sum[x | x <- [1..1000], mod x 5 == 0 || mod x 3 == 0, x < 1000]
+
+
+prodsOdd = [x*y | x <- [1..3], y <- [2..5], mod (x+y) 2 == 1 ]
+
 
 cartProd xs ys = [(x,y) | x <- xs, y <- ys]
 
+
 map f xs = [f x | x <- xs]
 
-filter p xs = [ x | x <- xs, p x ]
+
+filter p xs = [x | x <- xs, p x]
+
 
 
